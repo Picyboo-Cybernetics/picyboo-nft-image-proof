08c025cae571a7ff00eccea829e532351cb6a47a
Phraya <contact@picyboo.com>
2025-11-01 23:10:20 +0700

    Update .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,19 @@
 // .eslintrc.cjs
 module.exports = {
   extends: ['standard'],
-  env: {
-    es2023: true,
-    node: true,
-    jest: true,
-  },
+  env: { es2023: true, node: true, jest: true },
   rules: {
     semi: ['error', 'always'],
     'comma-dangle': 'off',
+    // erzwinge kein Leerzeichen
     'space-before-function-paren': ['error', 'never'],
   },
+  overrides: [
+    {
+      files: ['**/*.js'],
+      rules: {
+        'space-before-function-paren': ['error', 'never'],
+      },
+    },
+  ],
 };
